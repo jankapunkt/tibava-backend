a2e44496b4d3242d81013085d3fe189a7e456751
e-mb <eric_mueller89@hotmail.com>
2022-05-03 16:04:26 +0200

    bug fix while importing annotations

--- backend/views/timeline_import.py
+++ backend/views/timeline_import.py
@@ -74,7 +74,9 @@ class TimelineImportEAF(View):
                             timeline=timeline_db, start=segment["start"], end=segment["end"]
                         )
 
-                        annotation_db, created = Annotation.objects.get_or_create(name=segment["label"], video=video_db)
+                        annotation_db, created = Annotation.objects.get_or_create(
+                            name=segment["label"], video=video_db, owner=request.user
+                        )
                         print(annotation_db.name)
                         tsa_db = TimelineSegmentAnnotation.objects.create(
                             timeline_segment=segment_db, annotation=annotation_db
