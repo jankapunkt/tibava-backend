55523a196890531c56e0ad2924b41e646f7261f6
Matthias Springstein <matthias.springstein@gmail.com>
2022-10-28 10:35:59 +0200

    Add a script to automatically add new plugin runs.

---
+++ backend/management/commands/run_plugins.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from backend.models import Video
+
+from backend.plugin_manager import PluginManager
+
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--video_ids", nargs="+", type=str)
+        parser.add_argument("--plugin", type=str)
+
+    def handle(self, *args, **options):
+        for video_id in options["video_ids"]:
+
+            try:
+                video_db = Video.objects.get(pk=video_id)
+                user_db = video_db.owner
+            except Video.DoesNotExist:
+                raise CommandError('Poll "%s" does not exist' % video_id)
+
+            plugin_manager = PluginManager()
+
+            plugin_manager(options["plugin"], [], user=user_db, video=video_db)
+
+            self.stdout.write(self.style.SUCCESS('Successfully start plugin "%s"' % video_id))
