5712df35c6d4f5bf6ea5186d35808f87c227f244
Christian Otto Home_Office_Desktop <chro@outlook.de>
2023-06-05 12:13:51 +0200

    empty annotation timelines now output boundaries with the respective shotnumber as value. you can choose the aggregation method between min, max and mean.

--- backend/views/video_export.py
+++ backend/views/video_export.py
@@ -18,8 +18,9 @@ from xml.etree import cElementTree as etree
 from io import StringIO
 
 
-from backend.models import Video, Annotation, AnnotationCategory, Timeline
-
+from backend.models import Video, Annotation, AnnotationCategory, Timeline, TimelineSegment, PluginRunResult
+from analyser.data import DataManager, Shot
+import numpy as np
 
 def time_to_string(sec, loc="en"):
     sec, sec_frac = divmod(sec, 1)
@@ -339,30 +340,104 @@ class VideoExport(View):
         eaf = Eaf(author="")
         eaf.remove_tier("default")
         eaf.add_linked_file(file_path=f"{video_db.id.hex}.mp4", mimetype="video/mp4")
-        # specific timeline
-        # for timeline_id in request.GET.get("timeline_ids"):
-        for timeline_db in Timeline.objects.filter(video=video_db, type=Timeline.TYPE_ANNOTATION):
+        
+        # get the boundary information from the timeline selected in parameters
+        try:
+            shot_timeline_db = Timeline.objects.get(id=parameters.get("shot_timeline_id"))
+        except Timeline.DoesNotExist:
+            raise Exception
+        
+        aggregation = ["max", "min", "mean"][parameters.get("aggregation")]
+
+        # if the timeline is not of type annotation, raise an Exception
+        if shot_timeline_db.type != Timeline.TYPE_ANNOTATION:
+            raise Exception
+        
+        # get the shots from the boundary timeline
+        shots = []
+        shot_timeline_segments = TimelineSegment.objects.filter(timeline=shot_timeline_db)
+        for x in shot_timeline_segments:
+            shots.append(Shot(start=x.start, end=x.end))
+
+        data_manager = DataManager("/predictions/")
+
+        # for all timelines
+        for timeline_db in Timeline.objects.filter(video=video_db):
             tier = timeline_db.name
+
             # ignore timelines with the same name TODO: check if there is a better way
             if tier in list(eaf.tiers.keys()):
                 continue
             eaf.add_tier(tier_id=tier)
             # store all annotations
+            
+            tl_type = timeline_db.plugin_run_result
+            
+            # if the type of the timeline is scalar convert it to elan format
+            if tl_type is not None:
+                # if it is not of type SCALAR, skip it
+                if tl_type.type != PluginRunResult.TYPE_SCALAR:
+                    continue
 
-            for segment_db in timeline_db.timelinesegment_set.all():
-                start_time = int(segment_db.start * 1000)
-                end_time = int(segment_db.end * 1000)
-                annotations = []
-                for segment_annotation_db in segment_db.timelinesegmentannotation_set.all():
-                    category = segment_annotation_db.annotation.category
-                    name = segment_annotation_db.annotation.name
-                    if category is not None:
-                        anno = f"{category.name}:{name}"
+                scalar_data = data_manager.load(timeline_db.plugin_run_result.data_id)
+                with scalar_data:
+                    y = np.asarray(scalar_data.y)
+                    time = np.asarray(scalar_data.time)
+                    for i, shot in enumerate(shots):
+                        annotations = []
+                        shot_y_data = y[np.logical_and(time >= shot.start, time <= shot.end)]
+
+                        if len(shot_y_data) <= 0:
+                            continue
+
+                        y_agg = 0
+                        
+                        if aggregation == "mean":
+                            y_agg = np.mean(shot_y_data)
+                        if aggregation == "max":
+                            y_agg = np.max(shot_y_data)
+                        if aggregation == "min":
+                            y_agg = np.min(shot_y_data)
+                        
+                        start_time = int(shot.start * 1000)
+                        end_time = int(shot.end * 1000)
+                        anno = str(round(float(y_agg), 3))
+
+                        annotations.append(f"value:{anno}")
+                        if len(annotations) > 0:
+                            eaf.add_annotation(tier, start=start_time, end=end_time, value=", ".join(annotations))
+            # if it is an annotation timeline already, just export it
+            else:
+                for segment_db in timeline_db.timelinesegment_set.all():
+                    start_time = int(segment_db.start * 1000)
+                    end_time = int(segment_db.end * 1000)
+                    annotations = []
+                    # if the timeline contains annotations, export them
+                    if len(segment_db.timelinesegmentannotation_set.all()) > 0:
+                        for segment_annotation_db in segment_db.timelinesegmentannotation_set.all():
+                            category = segment_annotation_db.annotation.category
+                            name = segment_annotation_db.annotation.name
+                            if category is not None:
+                                anno = f"{category.name}:{name}"
+                            else:
+                                anno = f"{name}"
+                            annotations.append(anno)
+                            # check why this occurs
+                            if start_time == end_time: 
+                                continue
+                            eaf.add_annotation(tier, start=start_time, end=end_time, value=", ".join(annotations))
                     else:
-                        anno = f"{name}"
-                    annotations.append(anno)
-                if len(annotations) > 0:
-                    eaf.add_annotation(tier, start=start_time, end=end_time, value=", ".join(annotations))
+                        # if it does not contain annotations, export the boundaries with placeholder values (here: shot number)
+                        for i, shot in enumerate(shots):
+                            annotations = []
+                            start_time = int(shot.start * 1000)
+                            end_time = int(shot.end * 1000)
+                            annotations.append(f"value:{i}")
+                            # check why this occurs
+                            if start_time == end_time: 
+                                continue
+                            eaf.add_annotation(tier, start=start_time, end=end_time, value=", ".join(annotations))
+
         # if request.GET.get("format") == "textgrid":
         #    textgrid = eaf.to_textgrid()
         # else:
