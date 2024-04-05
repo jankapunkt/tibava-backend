579738b0959adcb93a25197fbcc8a02a601fb5bf
Matthias Springstein <matthias.springstein@gmail.com>
2024-04-05 10:52:17 +0200

    Add BLIP again and fix some other issues with BLIP.

--- backend/tasks/blip_vqa.py
+++ backend/tasks/blip_vqa.py
@@ -8,11 +8,16 @@ from backend.plugin_manager import PluginManager
 from backend.utils import media_path_to_video
 from backend.utils.parser import Parser
 from backend.utils.task import Task
-from analyser.data import DataManager
-from backend.models import AnnotationCategory, TimelineSegment, Annotation, TimelineSegmentAnnotation, TibavaUser
+from analyser.data import Shot, DataManager
+from backend.models import (
+    AnnotationCategory,
+    TimelineSegment,
+    Annotation,
+    TimelineSegmentAnnotation,
+    TibavaUser,
+)
 from django.db import transaction
 from django.conf import settings
-
 
 
 @PluginManager.export_parser("blip_vqa")
@@ -20,9 +25,8 @@ class BLIPVQAParser(Parser):
     def __init__(self):
         self.valid_parameter = {
             "timeline": {"parser": str, "default": "blip_vqa"},
+            "shot_timeline_id": {"required": True},
             "query_term": {"parser": str, "required": True},
-            "fps": {"parser": float, "default": 2.0},
-            "normalize": {"parser": float, "default": 1},
         }
 
 
@@ -36,7 +40,12 @@ class BLIPVQA(Task):
         }
 
     def __call__(
-        self, parameters: Dict, user: TibavaUser = None, video: Video = None, plugin_run: PluginRun = None, **kwargs
+        self,
+        parameters: Dict,
+        user: TibavaUser = None,
+        video: Video = None,
+        plugin_run: PluginRun = None,
+        **kwargs
     ):
         manager = DataManager(self.config["output_path"])
         client = TaskAnalyserClient(
@@ -47,13 +56,47 @@ class BLIPVQA(Task):
         )
 
         video_id = self.upload_video(client, video)
+
+        shots_id = None
+        if parameters.get("shot_timeline_id"):
+            shot_timeline_db = Timeline.objects.get(
+                id=parameters.get("shot_timeline_id")
+            )
+            shot_timeline_segments = TimelineSegment.objects.filter(
+                timeline=shot_timeline_db
+            )
+
+            shots = manager.create_data("ShotsData")
+            with shots:
+                for x in shot_timeline_segments:
+                    shots.shots.append(Shot(start=x.start, end=x.end))
+            shots_id = client.upload_data(shots)
+
+        result = self.run_analyser(
+            client,
+            "timeline_video_sampler",
+            parameters={
+                "middle_frame": True,
+                "start_frame": False,
+                "end_frame": False,
+            },
+            inputs={"input": video_id, "shots": shots_id},
+            outputs=["output"],
+        )
+        if result is None:
+            raise Exception
+        
         result = self.run_analyser(
             client,
             "blip_image_embedding",
-            parameters={"fps": parameters.get("fps")},
-            inputs={"video": video_id},
+            parameters={},
+            inputs={"input": result[0]["output"]},
             outputs=["embeddings"],
         )
+
+        if result is None:
+            raise Exception
+        
         plugin_run.progress = 0.5
         plugin_run.save()
 
@@ -64,7 +107,7 @@ class BLIPVQA(Task):
             client,
             "blip_vqa",
             parameters={"query_term": parameters.get("query_term")},
-            inputs={**result[0]},
+            inputs={**result[0], "shots": shots_id},
             downloads=["annotations"],
         )
         if result is None:
@@ -72,22 +115,30 @@ class BLIPVQA(Task):
 
         with transaction.atomic():
             with result[1]["annotations"] as data:
+                
+                # print("A", flush=True)
                 """
                 Create a timeline labeled
                 """
                 # print(f"[{PLUGIN_NAME}] Create annotation timeline", flush=True)
                 annotation_timeline_db = Timeline.objects.create(
-                    video=video, name=parameters.get("timeline"), type=Timeline.TYPE_ANNOTATION
+                    video=video,
+                    name=parameters.get("timeline"),
+                    type=Timeline.TYPE_ANNOTATION,
                 )
 
-                category_db, _ = AnnotationCategory.objects.get_or_create(name="Blib", video=video, owner=user)
+                category_db, _ = AnnotationCategory.objects.get_or_create(
+                    name="Blib", video=video, owner=user
+                )
 
+                # print("B", flush=True)
                 for annotation in data.annotations:
                     timeline_segment_db = TimelineSegment.objects.create(
                         timeline=annotation_timeline_db,
                         start=annotation.start,
                         end=annotation.end,
                     )
+                    # print(f"C {annotation.start} {annotation.end}", flush=True)
                     for label in annotation.labels:
                         annotation_db, _ = Annotation.objects.get_or_create(
                             name=str(label),
@@ -96,14 +147,16 @@ class BLIPVQA(Task):
                             owner=user,
                             # color=color,
                         )
+                        # print(f"D {str(label)}", flush=True)
 
                         TimelineSegmentAnnotation.objects.create(
-                            annotation=annotation_db, timeline_segment=timeline_segment_db
+                            annotation=annotation_db,
+                            timeline_segment=timeline_segment_db,
                         )
 
                 return {
                     "plugin_run": plugin_run.id.hex,
                     "plugin_run_results": [],
-                    "timelines": {"annotations":annotation_timeline_db.id.hex},
-                    "data": {"annotations": result[1]["annotations"].id}
+                    "timelines": {"annotations": annotation_timeline_db.id.hex},
+                    "data": {"annotations": result[1]["annotations"].id},
                 }
